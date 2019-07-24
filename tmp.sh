7d87f1f96f4f61bfe34e789c063e3d36c630a67b
jzijin <jzijin@163.com>
2019-07-24 22:40:14 +0800

    add 18.04 solution

--- tmp.sh
+++ tmp.sh
@@ -12,3 +12,4 @@ sudo docker tag kry1702/kube-proxy:v1.15.0 k8s.gcr.io/kube-proxy:v1.15.0
 sudo docker tag kry1702/pause:3.1 k8s.gcr.io/pause:3.1
 sudo docker tag kry1702/etcd:3.3.10 k8s.gcr.io/etcd:3.3.10
 sudo docker tag kry1702/coredns:1.3.1 k8s.gcr.io/coredns:1.3.1
+https://blog.csdn.net/liukuan73/article/details/83116271
